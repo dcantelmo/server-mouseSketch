f413738d554146bf0def298b2eb083112064f8e0
Daniele Cantelmo <danielecantelmo27@gmail.com>
2020-05-20 20:16:08 +0200

    fixed-initialize

--- initializeDB.js
+++ initializeDB.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 
 const db = new sqlite3.Database(
@@ -12,28 +14,50 @@ const db = new sqlite3.Database(
 dbrun();
 
 function dbrun() {
-    db.run(
-        `CREATE TABLE IF NOT EXISTS user(
-    pid INTEGER PRIMARY KEY AUTOINCREMENT,
-    nickname TEXT UNIQUE,
-    email TEXT UNIQUE,
-    hash text)`,
-        (error) => {
-            if (error) console.log(error.message);
-            else console.log('tabella creata');
+    db.run(`DROP TABLE IF EXISTS user`, (error) => {
+        if (error) console.log(error.message);
+        else {
+            db.run(
+                `CREATE TABLE IF NOT EXISTS user(
+                pid INTEGER PRIMARY KEY AUTOINCREMENT,
+                nickname TEXT UNIQUE,
+                email TEXT UNIQUE,
+                hash text)`,
+                (error) => {
+                    if (error) console.log(error.message);
+                    else console.log('tabella user creata');
+                }
+            );
         }
-    );
-    db.run(
-        `CREATE TABLE IF NOT EXISTS image(
-    pid INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT,
-    author TEXT,
-    path TEXT UNIQUE,
-    FOREIGN KEY (author) REFERENCES user(nickname))`,
-        (error) => {
-            if (error) console.log(error.message);
-            else console.log('tabella creata');
+    });
+
+    db.run(`DROP TABLE IF EXISTS image`, (error) => {
+        if (error) console.log(error.message);
+        else {
+            db.run(
+                `CREATE TABLE IF NOT EXISTS image(
+                pid INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                author TEXT,
+                path TEXT UNIQUE,
+                FOREIGN KEY (author) REFERENCES user(nickname))`,
+                (error) => {
+                    if (error) console.log(error.message);
+                    else console.log('tabella image creata');
+                }
+            );
         }
-    );
-    
+    });
+
+    const directory = './images';
+
+    fs.readdir(directory, (err, files) => {
+        if (err) throw err;
+
+        for (const file of files) {
+            fs.unlink(path.join(directory, file), (err) => {
+                if (err) throw err;
+            });
+        }
+    });
 }
